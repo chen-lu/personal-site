97c8ce59aa25002650c0694f7b2b4461488fedcc
chen.lu <chenlu2015@u.northwestern.edu>
2016-08-07 20:17:31 -0400

    set up gulp

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    usemin = require('gulp-usemin'),
+    minifyCss = require('gulp-minify-css'),
+    clean = require('gulp-clean');
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', ['browserSync', 'less'],function(){
+  gulp.watch('app/less/**/*.less', ['less']);
+  gulp.watch('app/js/**/*.js', ['scripts']);
+  gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('less', function() {
+  gulp.src('app/less/**/*.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('app/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(browserSync.reload({
+      stream : true
+    }));
+});
+
+gulp.task('clean', function(){
+  return gulp.src('dist/', {read:false})
+    .pipe(clean());
+})
+
+//builds and minifies css/js files
+gulp.task('build', ['clean'],  function(){
+  gulp.src('app/index.html')
+    .pipe(usemin({
+      assetsDir: 'app',
+      css: [minifyCss(), 'concat'],
+      js: [uglify(), 'concat']
+    }))
+    .pipe(gulp.dest('dist'));
+})
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
