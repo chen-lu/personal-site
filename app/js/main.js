c5d8baf6395897d7809a528bb00d53d6a6002d1f
chen.lu <chenlu2015@u.northwestern.edu>
2016-08-10 21:51:13 -0400

    add fullpage bg, styling for nav, sections

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,10 @@ $( document ).ready(function() {
   var mn = $(".main-nav"),
       mns = "main-nav-scrolled",
       pad = 50,
-      hdr = $('.intro').height()-pad;
-
+      hdr = $('.full-page').height()-pad;
 
   $(window).resize(function(){
-    hdr = $('.intro').height()-pad;
+    hdr = $('.full-page').height()-pad;
   })
 
   $(window).scroll(function() {
@@ -16,4 +15,21 @@ $( document ).ready(function() {
       mn.removeClass(mns);
     }
   });
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - 80
+          }, 500);
+          return false;
+        }
+      }
+    });
+  });
+
+
 });
